3eb77e491f558b482b2bacfa0f95c1f193f9db0d
Jaewon Seul <jaewon_seul@m018659.aw.org>
2021-09-22 20:21:56 -0700

    upload

---
+++ pythonBasics.py
@@ -0,0 +1,27 @@
+def main():
+
+    #name and date
+    print("Python Basic Commands Practice")
+    print("9/22/2021")
+
+    
+    var1= 30
+    var2= 69
+    #sum of the variables
+    print (var1+var2)
+
+    #product of the variables 
+    print (var1*var2)
+
+    #numbers of students
+    stunum1=20
+    stunum2=80
+    rlstunum= 114
+    print("There are currently "+ str(stunum1)+" number of students in the USB")
+    print("There are currently "+ str(stunum2)+" number of students in the USB")
+    print("There are currently "+ str(rlstunum) +" number of students in the USB")
+
+
+
+if __name__ == "__main__":
+    main()
